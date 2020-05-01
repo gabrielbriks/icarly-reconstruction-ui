1602e3e3a6d44d0b81a822bd613870dd2483d58c
gabrielbriks <greis9010@gmail.com>
2020-04-30 22:12:50 -0300

    Construindo estrutura inicial

---
+++ src/pages/Home/index.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react';
+import { GiMagnifyingGlass } from "react-icons/gi";
+
+import './styles.css';
+import logoImg from '../../assets/logo.png';
+import membrosICarly from '../../assets/membros-icarly.png';
+
+export default function Home(){
+  let date = new Date
+  console.log(date)
+  function handleSearch(){
+    console.log('clikked')
+  }
+  return (
+
+    <div className="container">
+       {/* <div className="imgLogo"> */}
+          <img src={logoImg} className="imgLogo" alt="Logo iCarly"/>
+        {/* </div> */}
+      <header className="imgsHeader">
+      <div className="test"></div>
+        
+        <div className="imgMembros">
+          <img src={membrosICarly} /*className="imgMembros"*/ alt="Membros iCarly"/>
+        </div>
+      </header>
+          
+      <div className="content">   
+        <section>
+          <div className="search">
+            <p className="todayIs">Today is </p>
+            <p className="phraseToDay">"LEMON TUBE"</p>           
+
+            <div>
+              <h3>search our site</h3>
+              <input type="text"/>
+              <button type="submit" onClick={handleSearch} className="btnSearch">
+                <GiMagnifyingGlass color={"#fff"} size={20}/>
+              </button>
+            </div>
+            
+            <div className="bntHeader">
+              <button className="iBlogs" >iBLOGS</button>
+              <button className="iSnaps">iSNAPS</button>
+              <button className="iStrivia">iTRIVIA</button>
+              <button>iVIDEO</button>
+            </div>
+            
+
+          </div>
+         
+        </section>
+      
+      </div>
+    </div>
+  );
+}
+
